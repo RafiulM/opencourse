02e2f08f4589fae7a2d557d3b9693b537f2697bc
Rafiul <ahmadrafiulm@gmail.com>
2025-10-07 17:11:50 +0700

    fix: frontend middleware session check

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -1,20 +1,23 @@
-import { getSessionCookie } from "better-auth/cookies"
 import { NextRequest, NextResponse } from "next/server"
+import { headers } from "next/headers"
+import { authClient } from "@/lib/auth"
 
 export async function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl
-    const session = await getSessionCookie(request)
-    console.log("middleware session", session)
-    console.log(session)
-    if (pathname.startsWith("/dashboard") && !session) {
-        return NextResponse.redirect(new URL("/login", request.url))
-    }
-    return NextResponse.next()
+  const { pathname } = request.nextUrl
+
+  const session = await authClient.getSession({
+    fetchOptions: {
+      headers: await headers(),
+    },
+  })
+
+  if (pathname.startsWith("/dashboard") && !session) {
+    return NextResponse.redirect(new URL("/login", request.url))
+  }
+  return NextResponse.next()
 }
 
-
 export const config = {
-    matcher: [
-        "/dashboard/:path*",
-    ]
+  runtime: "nodejs",
+  matcher: ["/dashboard/:path*"],
 }
